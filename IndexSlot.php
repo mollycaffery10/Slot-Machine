0385d6814e76788fb51f1ec2363595caa5492e4a
Molly Caffery <mollycaffery10@gmail.com>
2021-10-24 19:45:17 -0400

    Baseline Randomizing
    Randomizing all slot numbers. WIP needing to add check for win.

--- IndexSlot.php
+++ IndexSlot.php
@@ -4,52 +4,33 @@
 <html>
 <head>
     <title>Slot Machine</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
-<h1>Slot Machine</h1>
+    <h1>Slot Machine</h1>
 <br>
 
 <div class="row">
-    <span id="row1"></span>
+    <span id="row1">0</span>
     <span id="row2">2</span>
     <span id="row3">0</span>
 </div>
 
 <div class="row">
-    <span id="row1"></span>
-    <span id="row2">0</span>
-    <span id="row3">1</span>
+    <span id="rowMid1">2</span>
+    <span id="rowMid2">0</span>
+    <span id="rowMid3">1</span>
 </div>
 
 <div class="row">
-    <span id="row1"></span>
-    <span id="row2">1</span>
-    <span id="row3">0</span>
+    <span id="rowLow1">1</span>
+    <span id="rowLow2">1</span>
+    <span id="rowLow3">0</span>
 </div>
 
 <button id ="lever" onclick=changeNumbers(document.getElementsByClassName('row'))>Pull Lever</button>
 
-<script>
-    $("#lever").on("click",function(){
-        $("span").text(function(i,txt){
-            switch(txt){
-                case "0" : $(this).text("2"); break;
-                case "1" : $(this).text("1"); break;
-                case "2" : $(this).text("0"); break;
-            }
-        });
-    });
-</script>
-
-<script>
-    function changeNumbers(rows) {
-        document.getElementById('row1').innerHTML = getRndInteger(0, 2);
-    }
-
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-</script>
+<script src="slotFunction.js"></script>
 
 </html>
 
--- slotFunction.js
+++ slotFunction.js
@@ -1,8 +1,17 @@
-class slotFunctionality{
-    constructor() {
-    }
+function changeNumbers(rows) {
+    document.getElementById('row1').innerHTML = getRndInteger(0, 2);
+    document.getElementById('row2').innerHTML = getRndInteger(0, 2);
+    document.getElementById('row3').innerHTML = getRndInteger(0, 2);
 
-    getRow(){
-    }
+    document.getElementById('rowMid1').innerHTML = getRndInteger(0, 2);
+    document.getElementById('rowMid2').innerHTML = getRndInteger(0, 2);
+    document.getElementById('rowMid3').innerHTML = getRndInteger(0, 2);
 
+    document.getElementById('rowLow1').innerHTML = getRndInteger(0, 2);
+    document.getElementById('rowLow2').innerHTML = getRndInteger(0, 2);
+    document.getElementById('rowLow3').innerHTML = getRndInteger(0, 2);
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
